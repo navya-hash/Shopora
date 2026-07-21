5c8eca8dde18a49b8f0f3a6da242173db8592378
navya-hash <navyamodi288@gmail.com>
2026-07-22 00:19:18 +0530

    navbar updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,32 +1,34 @@
 'use client'
+
 import { Search, ShoppingCart, PackageIcon } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import {useUser,useClerk,UserButton} from "@clerk/nextjs";
-
+import { useUser, useClerk, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
 
     const router = useRouter();
-    const {user} = useUser();
-    const {openSignIn} = useClerk();
-    const [search, setSearch] = useState('')
-    const cartCount = useSelector(state => state.cart.total)
+    const { user } = useUser();
+    const { openSignIn } = useClerk();
+    const [search, setSearch] = useState('');
+    const cartCount = useSelector((state) => state.cart.total);
 
     const handleSearch = (e) => {
-        e.preventDefault()
-        router.push(`/shop?search=${search}`)
-    }
+        e.preventDefault();
+        router.push(`/shop?search=${search}`);
+    };
 
     return (
         <nav className="relative bg-white">
             <div className="mx-6">
-                <div className="flex items-center justify-between max-w-7xl mx-auto py-4  transition-all">
+                <div className="flex items-center justify-between max-w-7xl mx-auto py-4 transition-all">
 
                     <Link href="/" className="relative text-4xl font-semibold text-slate-700">
-                        <span className="text-green-600">go</span>cart<span className="text-green-600 text-5xl leading-0">.</span>
+                        <span className="text-green-600">go</span>cart
+                        <span className="text-green-600 text-5xl leading-0">.</span>
+
                         <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-green-500">
                             plus
                         </p>
@@ -39,53 +41,80 @@ const Navbar = () => {
                         <Link href="/">About</Link>
                         <Link href="/">Contact</Link>
 
-                        <form onSubmit={handleSearch} className="hidden xl:flex items-center w-xs text-sm gap-2 bg-slate-100 px-4 py-3 rounded-full">
+                        <form
+                            onSubmit={handleSearch}
+                            className="hidden xl:flex items-center w-xs text-sm gap-2 bg-slate-100 px-4 py-3 rounded-full"
+                        >
                             <Search size={18} className="text-slate-600" />
-                            <input className="w-full bg-transparent outline-none placeholder-slate-600" type="text" placeholder="Search products" value={search} onChange={(e) => setSearch(e.target.value)} required />
+                            <input
+                                className="w-full bg-transparent outline-none placeholder-slate-600"
+                                type="text"
+                                placeholder="Search products"
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                                required
+                            />
                         </form>
 
                         <Link href="/cart" className="relative flex items-center gap-2 text-slate-600">
                             <ShoppingCart size={18} />
                             Cart
-                            <button className="absolute -top-1 left-3 text-[8px] text-white bg-slate-600 size-3.5 rounded-full">{cartCount}</button>
+                            <span className="absolute -top-1 left-3 text-[8px] text-white bg-slate-600 size-3.5 rounded-full flex items-center justify-center">
+                                {cartCount}
+                            </span>
                         </Link>
+
                         {!user ? (
-                        <button onClick={()=>{openSignIn()}} className="px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
-                            Login
-                        </button>
+                            <button
+                                onClick={() => openSignIn()}
+                                className="px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full"
+                            >
+                                Login
+                            </button>
                         ) : (
-                            //here add the user button from clerk
-                        <UserButton >
-                            <UserButton.MenuItems align="end">
-                                <UserButton.Action labelIcon={<PackageIcon size={16}/>} label="My Orders" onClick={()=> router.push('/orders')}/>
-                            </UserButton.MenuItems>
-                        </UserButton>
+                            <UserButton>
+                                <UserButton.MenuItems align="end">
+                                    <UserButton.Action
+                                        labelIcon={<PackageIcon size={16} />}
+                                        label="My Orders"
+                                        onClick={() => router.push("/orders")}
+                                    />
+                                </UserButton.MenuItems>
+                            </UserButton>
                         )}
                     </div>
 
-                    {/* Mobile User Button  */}
-                    {user ?(
-                        <UserButton >
+                    {/* Mobile */}
+                    {user ? (
+                        <UserButton>
                             <UserButton.MenuItems align="end">
-                                <UserButton.Action labelIcon={<ShoppingCart size={16}/>} label="Cart" onClick={()=> router.push('/cart')}/>
-                                <UserButton.Action labelIcon={<PackageIcon size={16}/>} label="My Orders" onClick={()=> router.push('/orders')}/>
+                                <UserButton.Action
+                                    labelIcon={<ShoppingCart size={16} />}
+                                    label="Cart"
+                                    onClick={() => router.push("/cart")}
+                                />
+                                <UserButton.Action
+                                    labelIcon={<PackageIcon size={16} />}
+                                    label="My Orders"
+                                    onClick={() => router.push("/orders")}
+                                />
                             </UserButton.MenuItems>
                         </UserButton>
-                         
-                    ):
-
-                    (<button onClick={()=>{openSignIn()}} className="sm:hidden">
-                        <button className="px-7 py-1.5 bg-indigo-500 hover:bg-indigo-600 text-sm transition text-white rounded-full">
+                    ) : (
+                        <button
+                            onClick={() => openSignIn()}
+                            className="sm:hidden px-7 py-1.5 bg-indigo-500 hover:bg-indigo-600 text-sm transition text-white rounded-full"
+                        >
                             Login
                         </button>
-                    </button>)}
-
+                    )}
 
                 </div>
             </div>
+
             <hr className="border-gray-300" />
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
